1df3debb7cba2fabcefc066bcfa409a7e75766a1
Red0catmk49 <134488047+Red0catmk49@users.noreply.github.com>
2024-03-08 18:04:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-souxzg
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Red0catmk49/vitejs-vite-souxzg)
---
+++ main.js
@@ -0,0 +1,63 @@
+/**
+ * const,let等の変数宣言
+ */
+// var val1 = 'var変数';
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = 'var変数を上書き';
+// console.log(val1);
+
+// // var変数を再宣言可能
+// var var1 = 'var変数を再宣言';
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// letは再現不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// //constは再宣言不可能
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: 'あかにゃこ',
+//   age: 23,
+// };
+// val4.name = 'aka_nyaco';
+// val4.address = "Miyazaki";
+// console.log(val4);
+
+// // constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "frog";
+// val5.push("ant");
+// console.log(val5);
+
+/**
+ * テンプレート文字列　``
+ */
+const name = 'あかにゃこ';
+const age = 23;
+
+// //「私の名前はあかにゃこです。年齢は23歳です。」
+
+// // 従来の方法
+// const message1 = '私の名前は' + name + 'です。年齢は' + age + '歳です。';
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
