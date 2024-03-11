1ecdd44c47ca070e7ada615f5e9dd63eccd6eeac
Red0catmk49 <hm19049@student.miyazaki-u.ac.jp>
2024-03-11 16:05:30 +0900

    学習レベルを昇格(分割代入)

--- main.js
+++ main.js
@@ -66,22 +66,37 @@
  * アロー関数
  */
 // 従来の関数
-function func1(str) {
-    return str;
-}
-// const func1 = function (str) {
+// function func1(str) {
+//     return str;
+// }
+// // const func1 = function (str) {
+// //     return str;
+// // };
+// console.log(func1("func1です"));
+
+// // //アロー関数
+// const func2 = (str) => {
 //     return str;
 // };
-console.log(func1("func1です"));
+// console.log(func2("func2です"));
 
-// //アロー関数
-const func2 = (str) => {
-    return str;
+// const func3 = (num1, num2) => ({
+//     hoge: num1,
+//     huga: num2,
+// });
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+const myProfile = {
+    name: "あかにゃこ",
+    age: 23
 };
-console.log(func2("func2です"));
 
-const func3 = (num1, num2) => ({
-    hoge: num1,
-    huga: num2,
-});
-console.log(func3(10, 20));
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+const {name, age} = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳でぇす。`;
+console.log(message2);
